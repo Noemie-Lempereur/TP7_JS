d27791d810a25384c9464b8d8e6a4c0fc0e35bd4
Noemie-Lempereur <lempereur.noemie@gmail.com>
2020-05-15 09:25:02 +0200

    fini

--- smiley.js
+++ smiley.js
@@ -38,6 +38,7 @@ function canvasApp(){
 
 function displayDateTime(){
     let a=document.body;
+    let b=document.getElementById("id1");
     let date = new Date();
     let jour=date.getDate();
     let mois=date.getMonth();
@@ -49,16 +50,57 @@ function displayDateTime(){
     let jour_semaine=new Array('dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi');
     let mois_noms=new Array('décembre', 'janvier', 'février', 'mars', 'avril', 'mai','juin','juillet','août','septembre','octobre','novembre');
     let text=document.createElement("p");
+    text.id="id2";
     text.innerHTML="Nous sommes le "+jour_semaine[day]+" "+jour+" "+mois_noms[mois]+" "+annee;
     let text2=document.createElement("p");
+    text2.id="id3";
     text2.innerHTML="Il est actuellement "+heures+":"+minutes+":"+secondes;
-    a.appendChild(text);
-    text.appendChild(text2);
+    a.insertBefore(text2,b);
+    a.insertBefore(text,text2);
+}
+
+function displaynewTime(){
+    let old=document.querySelector("#id3");
+    let a=document.body;
+    let date = new Date();
+    let secondes=date.getSeconds();
+    let minutes=date.getMinutes();
+    let heures=date.getHours();
+    let text2=document.createElement("p");
+    if(secondes<10){
+        secondes="0"+secondes;
+    }
+    text2.innerHTML="Il est actuellement "+heures+":"+minutes+":"+secondes;
+    a.replaceChild(text2,old);
+    old.innerHTML=""
+    text2.id="id3";
+}
+
+function actualisation() {
+    temps--;
+    decre.innerText="Le dessin va s'afficher après "+temps;
+    displaynewTime();
+}
+
+
+function displayDecrementSeconds(time){
+    let temps=58;
+    let a=document.body;
+    let b=document.getElementById("id1");
+    let decre=document.createElement("p");
+    decre.innerText="Le dessin va s'afficher après "+temps;
+    let actualisation=setInterval(actualisation(), 1000);
+    a.insertBefore(decre,b);
+    setTimeout(function () {
+        clearInterval(actualisation);
+        decre.innerText = "Le dessin est affiché ci-dessous";
+        canvasApp();
+    }, 58000);
 }
 
 function main() {
-    canvasApp();
     displayDateTime();
+    displayDecrementSeconds();
 }
 
 main();
